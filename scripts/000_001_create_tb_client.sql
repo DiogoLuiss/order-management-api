f664b2722ba4cc2b0e11c260b3958d18ed92442f
DiogoPML <diogoluisilva08@gmail.com>
2025-09-15 10:11:54 -0300

    making scripts generic

--- scripts/000_001_create_tb_client.sql
+++ scripts/000_001_create_tb_client.sql
@@ -1,13 +1,3 @@
-USE [order_management]
-GO
-
-/****** Object:  Table [dbo].[client]    Script Date: 11/09/2025 15:24:02 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
 CREATE TABLE [dbo].[client](
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[name] [nvarchar](255) NOT NULL UNIQUE,
@@ -19,9 +9,3 @@ CREATE TABLE [dbo].[client](
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[client] ADD  CONSTRAINT [DF_client_created_at]  DEFAULT (getdate()) FOR [created_at]
-GO
-
-
--- scripts/000_002_create_tb_product.sql
+++ scripts/000_002_create_tb_product.sql
@@ -1,12 +1,3 @@
-USE [order_management]
-GO
-
-/****** Object:  Table [dbo].[product]    Script Date: 11/09/2025 09:29:28 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
 
 CREATE TABLE [dbo].[product](
 	[id] [int] IDENTITY(1,1) NOT NULL,
@@ -19,6 +10,6 @@ CREATE TABLE [dbo].[product](
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
-GO
+
 
 
